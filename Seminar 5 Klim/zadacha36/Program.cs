fe0aa8b6e39999db380f42be91bfd2bf1d71cb55
Den <as_pioneer@mail.ru>
2022-07-14 00:07:09 +0600

    add zadacha 36, zadacha 38

---
+++ Seminar 5 Klim/zadacha36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Console.Write("Введите колличество элементов массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+int[] GetArrayRandom(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next();
+    }
+    return array;
+}
+
+void PrintArray(int[] GetArrayRandom)
+{
+    foreach (var item in GetArrayRandom)
+    {
+        Console.Write(item+" ");
+    }
+}
+int[] array2 = GetArrayRandom(size);
+PrintArray(array2);
+int sum = 0;
+for (int i = 0; i < array2.Length; i+=2)
+{
+    sum = sum + array2[i];
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях в массиве = {sum}");
---
+++ Seminar 5 Klim/zadacha38/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Console.Write("Введите колличество элементов массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+double[] GetArrayRandom(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().NextDouble();
+    }
+    return array;
+}
+
+void PrintArray(double[] GetArrayRandom)
+{
+     foreach (var item in GetArrayRandom)
+     {
+        Console.Write(item+" ");
+     }
+}
+double[] array2 = GetArrayRandom(size);
+PrintArray(array2);
+
+double min = array2[0];
+double max = array2[0];
+for (int i = 1; i < array2.Length; i++)
+{
+    if(array2[i] < min)
+    {
+        min = array2[i];
+    }
+    if(array2[i] > max)
+    {
+        max = array2[i];
+    }
+}
+
+double Difference = max - min;
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива = {Difference}");
